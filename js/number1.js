a61cc8f1e4d6bb17f1cc345761f8a739bebb99ab
unknown <prajna227@gmail.com>
2018-07-16 16:47:15 +0530

    functions added

--- js/number1.js
+++ js/number1.js
@@ -1,19 +1,32 @@
 function calc(operation)
 {
-    var num1 = parseInt(document.getElementById("res").value);
-     var num2 = parseInt(document.getElementById("res").value);
+    var num1 = parseInt(document.getElementById("num1").value);
+     var num2 = parseInt(document.getElementById("num2").value);
     var message = " ";
     switch(operation)
         {
             case 'add' :
-            message = num1 + "+" + num2+"="+(num1+num2);
+                  message = num1 + "+" + num2+"="+(num1+num2);
+                  break;
                 
             case 'sub' :
                 message = num1 + "+" + num2+"="+(num1-num2);
+                break;
             case 'mul' : 
                 message = num1 + "+" + num2+"="+(num1*num2);
+                break;
                 
-            case 'add' : 
-                message = num1 + "+" + num2+"="+(num1*num2);
+            case 'div' : 
+                message = num1 + "+" + num2+"="+(num1 / num2); 
+                break;
                 
+            case 'mod' : 
+                message = num1 + "+" + num2+"="+(num1 % num2);
+                break;
+         default : message = "enter a valid number";
+                
+                
+                
+}
+    document.getElementById("res").innerHTML = message;
 }
